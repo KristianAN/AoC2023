a74f986d912d63b655f4796f974358b27862ccdb
Kristian Nedrevold-Hansen <kristian@krined.no>
2023-12-05 16:00:26 +0100

    Brute force d5

--- scala/src/Day5.scala
+++ scala/src/Day5.scala
@@ -23,4 +23,35 @@ def solveOne(input: String) =
           case Some((source, target)) => seed + (target.min - source.min)
     .min
 
-def solveTwo(input: List[String]) = 1
+// brute force
+def solveTwo(input: String) =
+
+  val groups = input.split("\n\n").filter(_.nonEmpty)
+  val seeds = groups.head
+    .split(":")
+    .drop(1)
+    .head
+    .split("\\s+")
+    .filter(_.nonEmpty)
+    .map(_.toLong)
+    .toList
+    .grouped(2)
+    .flatMap: g =>
+      Range.Long(g(0), g(0) + g(1), 1)
+
+  groups.tail
+    .map: gr =>
+      gr.split("\n")
+        .tail
+        .map: mapping =>
+          val n = mapping.split("\\s+").filter(_.nonEmpty).map(_.toLong)
+          val source = Range.Long(n(1), n(1) + n(2), 1)
+          val target = Range.Long(n(0), n(0) + n(2), 1)
+          (source, target)
+    .foldLeft(seeds.toList): (acc, map) =>
+      acc.map: seed =>
+        map
+          .find(_._1.contains(seed)) match
+          case None                   => seed
+          case Some((source, target)) => seed + (target.min - source.min)
+    .min
